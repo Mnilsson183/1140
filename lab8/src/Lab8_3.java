228b98fe8de7eb1496e20f02108256192dddd942
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2023-11-02 12:09:34 -0600

    quick start

---
+++ lab8/src/Lab8_3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Lab8_3 {
+    public static void main(String[] args) {
+        //File file = new File("/home/morgan/Desktop/1140/lab8/src/Lab8Data.txt");
+        Scanner input = new Scanner();
+
+        System.out.println("");
+    }
+}
